cbfc3f532140cc507ea892bdb9624090d33bffe7
Gehr <TEVIS.GEHR@accudyneindustries.com>
2017-01-28 20:44:44 -0700

    branched for the 'rbf' kernel

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,13 +24,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #My code:
 #########################################################
 from sklearn import svm
-clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf')
 print "Fitting:"
 t = time()
 
 #added to cut down training time
 features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
 
 clf.fit(features_train,labels_train)
 print (time() - t), "s"
